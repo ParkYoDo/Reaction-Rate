ed920838c9b7001548bddc6e38747a63f4e02502
ParkYoDo <scarletdemon@naver.com>
2023-03-27 03:39:09 +0900

    netlify 배포 오류 수정

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import { GlobalStyle } from 'GlobalStyle/GlobalStyle';
 import { BrowserRouter } from 'react-router-dom';
 import App from 'App';
 
-const root = ReactDOM.createRoot(document.getElementById('root')!);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
     <BrowserRouter>
